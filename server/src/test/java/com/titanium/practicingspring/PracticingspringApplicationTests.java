58902626c7d2b810a0f5511e4af6b14d478655ad
Fredrick0K <92822229+Fredrick0K@users.noreply.github.com>
2026-05-21 10:05:44 +0200

    Submitted Server project

---
+++ server/src/test/java/com/titanium/practicingspring/PracticingspringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.titanium.practicingspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticingspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
