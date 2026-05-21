d5ccde7be9db3dc13bf3cfcfe61ed7d8c87e3ed7
Fredrick0K <92822229+Fredrick0K@users.noreply.github.com>
2026-05-21 10:10:41 +0200

    Add files via upload

---
+++ src/main/java/com/titanium/practicingspring/model/Reserva.java
@@ -0,0 +1,45 @@
+package com.titanium.practicingspring.model;
+
+import java.time.LocalDateTime;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "reserva")
+public class Reserva {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @ManyToOne
+    @JoinColumn(name = "id_usuario")
+    private Usuario usuario;
+
+    @ManyToOne
+    @JoinColumn(name = "id_pista")
+    private Pista pista;
+
+    @Column(name = "hora_inicio")
+    private LocalDateTime horaInicio;
+
+    @Column(name = "hora_fin")
+    private LocalDateTime horaFin;
+
+    private String estado;
+
+    @Column(name = "precio_total")
+    private Double precioTotal;
+
+    @Column(name = "fecha_creacion")
+    private LocalDateTime fechaCreacion;
+
+}
---
+++ src/main/java/com/titanium/practicingspring/model/Usuario.java
@@ -0,0 +1,47 @@
+package com.titanium.practicingspring.model;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "usuario")
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String nombre;
+
+    private String email;
+
+    private String telefono;
+
+    @Column(name = "contraseña")
+    private String contrasena;
+
+    private String rol;
+
+    @JsonProperty("activo")
+    private boolean activo;
+
+    @Column(name = "fecha_creacion")
+    private LocalDateTime fechaCreacion;
+
+    @OneToMany(mappedBy = "usuario")
+    @JsonIgnore
+    private List<Reserva> reservas;
+
+}
